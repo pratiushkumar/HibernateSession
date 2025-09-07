a018d508cd5c495958941d17248e14ad214d1a4f
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-07 22:12:56 +0530

    Patient

---
+++ src/main/java/com/hiber/hiber_demo/Patient.java
@@ -0,0 +1,36 @@
+package com.hiber.hiber_demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicUpdate(value=true)
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+
+    public Patient() {}
+
+    public Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
